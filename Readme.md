973f8a8a0d133861e8f5e125a58190541d5057c2
pravin-pk <19cs119.pravin@sjec.ac.in>
2022-05-20 16:06:23 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,17 @@
+__Requirements__ 
+* Python installed in system
+
+Follow the below steps to run the Flask web server
+
+1. `pip install -r requirements.txt`
+2. `python movieapp.py`
+
+The Web app will be running in `<your-ipAdress>:5000`
+
+<hr>
+
+To get all the details of all movies click on _query_ button without any input
+
+To insert new record into the database use the TextBoxes in the end of the page
+
+Thank you 😊🤗
---
+++ movieapp.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, redirect, url_for, request
+import sqlite3
+
+from sqlalchemy import null
+
+app = Flask(__name__)
+
+
+@app.route("/insert" , methods = ['POST', 'GET'])
+def insert():
+    try:
+        conn = sqlite3.connect("movieDB.db")
+        cur = conn.cursor()
+
+        cur.execute(
+            "INSERT INTO MOVIES(MOVIE, ACTOR, ACTRESS, YEAROFRELEASE, DIRECTOR) VALUES(?, ?, ?, ?, ?)",
+            (
+                request.form.get("movie"), 
+                request.form.get("actor"),
+                request.form.get("actress"),
+                request.form.get("yearofrelease"),
+                request.form.get("director")
+            ),
+        )
+        
+        cur.close()
+        conn.commit()
+        return redirect(url_for("home"))
+    except sqlite3.Error  as e:
+        print(e)
+        return e
+
+
+@app.route("/", methods = ['POST', 'GET'])
+def home():
+    if request.method != "POST":
+        conn = sqlite3.connect("movieDB.db")
+        cur = conn.cursor()
+
+        cur.execute("""
+                    CREATE TABLE IF NOT EXISTS MOVIES(
+                        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                        MOVIE TEXT,
+                        ACTOR TEXT,
+                        ACTRESS TEXT,
+                        YEAROFRELEASE TEXT,
+                        DIRECTOR TEXT
+                    )
+                    """)
+        conn.commit()
+
+        cur.execute("SELECT * FROM MOVIES")
+        data = cur.fetchall()
+        cur.close()
+
+        return render_template("home.html", content = data)
+
+    if request.form.get("queryactor") == '':
+        return redirect(url_for("home"))
+
+    conn = sqlite3.connect("movieDB.db")
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM MOVIES WHERE ACTOR LIKE '%" + request.form.get("queryactor") + "%';")
+    data = cur.fetchall()
+
+    cur.close()
+
+    return render_template("home.html", content = data)
+    
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
